e29301fdb77fb212f6f86a1d61e929803cfa92b6
Arnauld <arnauld.loyer@gmail.com>
2022-05-04 14:39:32 +0200

    feat: naive tcp listener based on tokyo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:7001";
+    println!("Starting Agent on port {}", addr);
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+        println!("new socket opened");
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                println!("bytes received {:?}", n);
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
 }
